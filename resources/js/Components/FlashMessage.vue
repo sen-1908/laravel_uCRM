0fd86012f4829f2984df2ee183a70ec90f416878
sen <sen121108@icloud.com>
2022-11-24 17:46:42 +0900

    createPage

---
+++ resources/js/Components/FlashMessage.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <p>{{$page.props.flash.status}}</p>
+    <div v-if="$page.props.flash.status === 'success'" class="bg-blue-300 text-white p4">
+        {{$page.props.flash.message}}
+    </div>
+</template>
