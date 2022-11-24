41bc523c080462a8e7031845c2ff83a3704c3f52
sen <sen121108@icloud.com>
2022-11-24 18:57:23 +0900

    test

--- resources/js/Pages/Items/Index.vue
+++ resources/js/Pages/Items/Index.vue
@@ -16,7 +16,7 @@ defineProps({
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">商品一覧</h2>
         </template>
-
+        <div></div>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
